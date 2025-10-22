aa3d2e353087b322b1b6e5b6f017259e1c569602
CodeX047 <vishalrp047@gmail.com>
2025-10-22 14:20:20 +0530

    feat(header): Add logout functionality
    - Implements a logout feature in the header.
    - Clears the 'loggedInUser' item from localStorage.
    - Reloads the window after logging out.

--- src/Other/Header.jsx
+++ src/Other/Header.jsx
@@ -1,15 +1,18 @@
 const Header = ({ data }) => {
-  
+  const LogOutUser = () => {
+    localStorage.setItem("loggedInUser", "");
+    window.location.reload();
+  };
 
   return (
     <div className="flex items-end justify-between pb-4 border-b border-gray-700">
       <h1 className="text-2xl font-medium text-gray-200">
         Hello, <br />
         <span className="text-3xl font-semibold text-white">
-          {!data ? 'Admin' : data.name} 👋
+          {!data ? "Admin" : data.name} 👋
         </span>
       </h1>
-      <button className="bg-red-600 hover:bg-red-700 transition-colors duration-200 text-white px-5 py-2 rounded-xl text-lg font-medium shadow-md shadow-red-900/30">
+      <button onClick={LogOutUser} className="bg-red-600 hover:bg-red-700 transition-colors duration-200 text-white px-5 py-2 rounded-xl text-lg font-medium shadow-md shadow-red-900/30">
         Log Out
       </button>
     </div>
